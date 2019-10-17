f591b93902efaa1d8b5ccee94151e8fdf02de418
Taha <tahamalek_m8@yahoo.com>
2019-10-17 21:45:38 +0330

    start of frontend

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png"/>
+        <div v-if="$apollo.loading">Loading..</div>
+        <div v-else>
+            <!--<p>Name: {{UserAdmin[0].name}}</p>-->
+            <!--<p>Email: {{UserAdmin.family }}</p>-->
+            <!--<p>phone: {{UserAdmin.phone }}</p>-->
+            <ul v-for="u in UserAdmin">
+            <li>{{u.name}}</li>
+            <li>{{u.phone}}</li>
+            <li>{{u.family}}</li>
+            </ul>
+        </div>
+
+        <!--<HelloWorld msg="Welcome to Your Vue.js App" />-->
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+    // import HelloWorld from "./components/HelloWorld.vue";
+    import {gql} from 'apollo-boost';
 
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+    export default {
+        // name: "app",
+        // components: {
+        //   HelloWorld
+        // },
+        data() {
+            return {
+                UserAdmin: {}
+            }
+        },
+        apollo: {
+            UserAdmin: {
+                query: gql`
+
+                   {
+                     UserAdmin
+                      {
+                      name
+                      family
+                        phone
+                      }
+                     }
+            `
+
+            }
+        }
+    };
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueApollo from "vue-apollo";
+import ApolloClient from 'apollo-boost';
+
+Vue.use(VueApollo);
+
+
+const client = new ApolloClient({
+  uri: "http://127.0.0.1:8001/graphql",
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: 'Bearer '+'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDEvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE1NzA2MzgzMzIsImV4cCI6MTU3MDY0MTkzMiwibmJmIjoxNTcwNjM4MzMyLCJqdGkiOiJKTFpxdXRqOG5iUWk4alh4Iiwic3ViIjoxLCJwcnYiOiI4N2UwYWYxZWY5ZmQxNTgxMmZkZWM5NzE1M2ExNGUwYjA0NzU0NmFhIn0.dmqZ6K48JLV4Y7HLEGN_04zrzCObBjjKu09YRDkka_A'
+      },
+    });
+  }
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: client,
+})
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  apolloProvider,
 }).$mount("#app");
